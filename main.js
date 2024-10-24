7176bfaf478fd259c52ee0b1aecf1ea5f50e9c47
Inma Gijón <igijoninf@gmail.com>
2024-10-24 13:42:43 +0200

    Obtener lista de usuarios de un endpoint

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Import all of Bootstrap's JS
 import './src/scss/styles.scss'
 import * as bootstrap from 'bootstrap'
-import { init } from "./src/js/chistes-page";
+import { obtenerUsuarios } from "./src/js/http-provider";
 
-init();
+obtenerUsuarios();
